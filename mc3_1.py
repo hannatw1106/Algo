caf9e2443c1c38860f03c92471e79de3324d04f3
AnnA <71726026+hannatw1106@users.noreply.github.com>
2025-04-05 12:16:23 +0300

    Add files via upload

---
+++ mc3_1.py
@@ -0,0 +1,186 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import* 
+from PyQt5.QtWidgets import*
+from random import*
+
+
+class Question():
+    ''' содержит вопрос, правильный ответ и три неправильных'''
+    def __init__(self, question, right_answer, wrong1, wrong2, wrong3):
+        self.question = question
+        self.right_answer = right_answer
+        self.wrong1 = wrong1
+        self.wrong2 = wrong2
+        self.wrong3 = wrong3
+
+
+questions_list = [] 
+questions_list.append(
+        Question('Государственный язык Бразилии', 'Португальский', 'Английский', 'Испанский', 'Бразильский'))
+questions_list.append(
+        Question('Какого цвета нет на флаге России?', 'Зелёный', 'Красный', 'Белый', 'Синий'))
+questions_list.append(
+        Question('Национальная хижина якутов', 'Ураса', 'Юрта', 'Иглу', 'Хата'))
+
+
+app = QApplication([])
+window = QWidget()
+
+window.setStyleSheet("background-color: rgb(145, 191, 126);")
+
+
+window.setWindowTitle('Memo Card')
+window.resize(400,200)
+window.move(100,100)
+
+lb_Question = QLabel('Самый сложный вопрос в мире!') # вопрос
+btn_OK = QPushButton('Ответить')
+
+RadioGroupBox = QGroupBox('Варианты ответов')
+rbtn_1 = QRadioButton('Вариант 1')
+rbtn_2 = QRadioButton('Вариант 2')
+rbtn_3 = QRadioButton('Вариант 3')
+rbtn_4 = QRadioButton('Вариант 4')
+
+
+RadioGroup = QButtonGroup() 
+RadioGroup.addButton(rbtn_1)
+RadioGroup.addButton(rbtn_2)
+RadioGroup.addButton(rbtn_3)
+RadioGroup.addButton(rbtn_4)
+
+
+layout_ans1 = QHBoxLayout()
+layout_ans2 = QVBoxLayout()
+layout_ans3 = QVBoxLayout()
+layout_ans2.addWidget(rbtn_1) #два ответа в левый столбец
+layout_ans2.addWidget(rbtn_2)
+layout_ans3.addWidget(rbtn_3)#два ответа в правый столбец
+layout_ans3.addWidget(rbtn_4)  
+
+#окно результата
+AnsGroupBox = QGroupBox("Результаты теста")
+lb_Result = QLabel('Правильно\Неправильно')
+lb_Correct = QLabel('Правильный ответ')
+
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result, alignment=(Qt.AlignLeft | Qt.AlignTop))
+layout_res.addWidget(lb_Correct, alignment=Qt.AlignHCenter)
+AnsGroupBox.setLayout(layout_res) #конец
+
+layout_ans1.addLayout(layout_ans2) #разместили столбцы в одной строке
+layout_ans1.addLayout(layout_ans3)
+RadioGroupBox.setLayout(layout_ans1)
+
+layout_line1 = QHBoxLayout()#вопрос
+layout_line2 = QHBoxLayout() # варианты ответов или результат теста
+layout_line3 = QHBoxLayout() #кнопка ответить
+
+
+layout_line1.addWidget(lb_Question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
+
+
+# Размещаем в одной строке обе панели, одна из них будет скрываться, другая показываться:
+layout_line2.addWidget(RadioGroupBox)   
+layout_line2.addWidget(AnsGroupBox)  
+AnsGroupBox.hide() 
+
+layout_line3.addStretch()
+layout_line3.addWidget(btn_OK)
+layout_line3.addStretch()
+
+
+layout_card = QVBoxLayout()
+layout_card.addLayout(layout_line1) #размещаем вопрос
+layout_card.addLayout(layout_line2,stretch = 1)
+layout_card.addLayout(layout_line3)
+layout_card.setSpacing(1)
+
+def show_result():
+    ''' показать панель ответов '''
+    RadioGroupBox.hide()
+    AnsGroupBox.show()
+    btn_OK.setText('Следующий вопрос')
+
+def show_question():
+    ''' показать панель вопросов '''
+    RadioGroupBox.show()
+    AnsGroupBox.hide()
+    btn_OK.setText('Ответить')
+    RadioGroup.setExclusive(False) # сняли ограничения, чтобы можно было сбросить выбор радиокнопки
+    rbtn_1.setChecked(False)
+    rbtn_2.setChecked(False)
+    rbtn_3.setChecked(False)
+    rbtn_4.setChecked(False)
+    RadioGroup.setExclusive(True) # вернули ограничения, теперь только одна радиокнопка может быть выбрана
+
+answers = [rbtn_1, rbtn_2, rbtn_3, rbtn_4]
+
+
+def ask(q: Question):
+    ''' функция записывает значения вопроса и ответов в соответствующие виджеты, 
+    при этом варианты ответов распределяются случайным образом'''
+    shuffle(answers) # перемешали список из кнопок, теперь на первом месте списка какая-то непредсказуемая кнопка
+    answers[0].setText(q.right_answer) # первый элемент списка заполним правильным ответом, остальные - неверными
+    answers[1].setText(q.wrong1)
+    answers[2].setText(q.wrong2)
+    answers[3].setText(q.wrong3)
+    lb_Question.setText(q.question) # вопрос
+    lb_Correct.setText(q.right_answer) # ответ 
+    show_question() # показываем панель вопросов 
+
+def show_correct(res):
+    ''' показать результат - установим переданный текст в надпись "результат" и покажем нужную панель '''
+    lb_Result.setText(res)
+    show_result()
+
+
+def check_answer():
+    ''' если выбран какой-то вариант ответа, то надо проверить и показать панель ответов'''
+    if answers[0].isChecked():
+        # правильный ответ!
+        show_correct('Правильно!')
+        window.score += 1
+        print('Статистика\n-Всего вопросов: ', window.total, '\n-Правильных ответов: ', window.score)
+        print('Рейтинг: ', (window.score/window.total*100), '%')
+    else:
+        if answers[1].isChecked() or answers[2].isChecked() or answers[3].isChecked():
+            # неправильный ответ!
+            show_correct('Неверно!')
+            print('Рейтинг: ', (window.score/window.total*100), '%')
+
+
+
+def next_question():
+    ''' задает случайный вопрос из списка '''
+    window.total += 1
+    print('Статистика\n-Всего вопросов: ', window.total, '\n-Правильных ответов: ', window.score)
+    cur_question = randint(0, len(questions_list) - 1)  # нам не нужно старое значение, 
+                                                        # поэтому можно использовать локальную переменную! 
+            # случайно взяли вопрос в пределах списка
+            # если внести около сотни слов, то редко будет повторяться
+    q = questions_list[cur_question] # взяли вопрос
+    ask(q) # спросили
+
+
+def click_OK():
+    ''' определяет, надо ли показывать другой вопрос либо проверить ответ на этот '''
+    if btn_OK.text() == 'Ответить':
+        check_answer() # проверка ответа
+    else:
+        next_question() # следующий вопрос
+
+
+
+
+window.setLayout(layout_card)
+
+btn_OK.clicked.connect(click_OK) # по нажатии на кнопку выбираем, что конкретно происходит
+
+
+window.score = 0
+window.total = 0
+next_question()
+
+window.show()
+app.exec()
